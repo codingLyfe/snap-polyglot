750d1069560c18acde82914d1e8793252f63037f
Tristan Bennett <tmbennett2011@msn.com>
2018-01-12 08:34:40 -0700

    updated symbol codes

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 	</head>
 	<body>
 		<h1>Snap Challenge Polyglot</h1>
-		<p>&iquestQui&eacuten fue unos de los poetas m&aacutes importantes en la historia de M&eacutexico? Sor Juana In&eacutes de la Cruz.
-			Uno de sus eleg&iacuteas m&aacutes importantes fue &laquoDe la beldad de Laura enemorados&raquo.</p>
+		<p>&iquest;Qui&eacute;n fue unos de los poetas m&aacute;s importantes en la historia de M&eacute;xico? Sor Juana In&eacute;s de la Cruz.
+			Uno de sus eleg&iacute;as m&aacute;s importantes fue &laquo;De la beldad de Laura enemorados&raquo;.</p>
 		<p>Who was on of the most important poets in the history of Mexico?  Sor Juana In&eacutes de la Cruz. One of her most
-		important selections was &laquoDe la beldad de Laura enemorados&raquo.</p>
+		important selections was &laquo;De la beldad de Laura enemorados&raquo;.</p>
 	</body>
 </html>
