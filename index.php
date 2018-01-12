0b06331a7276e6b08f166289484f92ba7a34febc
Tristan Bennett <tmbennett2011@msn.com>
2018-01-12 08:19:38 -0700

    created index.php and added 2 paragraphs

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Snap Challenge Polyglot</title>
+	</head>
+	<body>
+		<h1>Snap Challenge Polyglot</h1>
+		<p>&iquestQui&eacuten fue unos lde los poetas m&aacutes importantes en la historia de M&eacutexico? Sor Juana In&eacutes de la Cruz.
+			Uno de sus eleg&iacuteas m&aacutes importantes fue << De la beldad de Laura enemorados >>.</p>
+		<p>Who was on of the most important poets in the history of Mexico?  Sor Juana In&eacutes de la Cruz. One of her most
+		important selections was << De la beldad de Laura enemorados >>.</p>
+	</body>
+</html>
